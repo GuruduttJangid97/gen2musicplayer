416df355dff0457004c9203b4f62759d10f3f467
Anirudh Sharma <anirudhsharma885@outlook.com>
2024-01-24 01:17:49 +0530

    Pages included

--- gen2musicplayer/src/App.js
+++ gen2musicplayer/src/App.js
@@ -1,10 +1,12 @@
-import Header from "./Components/header";
-import Footer from "./Components/footer";
+import Home from "./Components/Pages/Home";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
